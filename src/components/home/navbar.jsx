a61668922b4b9f3fb3a658e03ca856dda57bff89
bavmeen <bavmeenkular@outlook.com>
2022-12-26 12:57:29 -0500

    homepageupdate

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-const Navbar = () =>{
-    return(
-        <div className="flex items-center  bg-slate-200 justify-between">
-            <div>
-                <img src="servici.png" alt="logo" className="w-28 p-2 " />
-            </div>
-            
-            <div className="border flex ">
-            <svg xmlns="http://www.w3.org/2000/svg" className="xl:w-8 w-4" viewBox="0 0 512 512"><path d="M511.2 387l-23.25 100.8c-3.266 14.25-15.79 24.22-30.46 24.22C205.2 512 0 306.8 0 54.5c0-14.66 9.969-27.2 24.22-30.45l100.8-23.25C139.7-2.602 154.7 5.018 160.8 18.92l46.52 108.5c5.438 12.78 1.77 27.67-8.98 36.45L144.5 207.1c33.98 69.22 90.26 125.5 159.5 159.5l44.08-53.8c8.688-10.78 23.69-14.51 36.47-8.975l108.5 46.51C506.1 357.2 514.6 372.4 511.2 387z"/></svg>
-             <span className="font-semibold md:text-lg xl:text-3xl ml-2 mb-1 text-sm">+91-2973-PICKUP</span>
-            </div>
-        </div>
-    );
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between  bg-slate-500 px-2 ">
+      <span>
+        <img src="servici.png" alt="logo" className="w-16" />
+      </span>
+      <form className="">
+        <input
+          type="search"
+          className=" p-2 pl-8   text-sm text-gray-900 bg-gray-50 rounded-full w-28 md:w-80  xl:w-96  border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="search"
+        />
+      </form>
+
+       <Link to="Client">
+       <div  className="relative px-5 py-2 font-medium text-white group">
+<span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-yellow-500 group-hover:bg-yellow-700 group-hover:skew-x-12"></span>
+<span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-yellow-700 group-hover:bg-yellow-500 group-hover:-skew-x-12"></span>
+<span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-yellow-600 -rotate-12"></span>
+<span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-yellow-400 -rotate-12"></span>
+ <span className="relative">Place Order</span>
+</div>
+
+        </Link>
+    </div>
+  );
 };
 export default Navbar;
